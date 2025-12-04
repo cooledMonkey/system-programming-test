21b426014f2d3107ef043dec4032ae933d055aba
Cooled Monkey <lemonacidic@mail.ru>
2025-12-04 17:48:19 +0300

    fix: delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-
-
-respone = aiohttp.ClientSession().get("")
-text = await response.json()
-soup = BeautifulSoup(response.content, 'html.parser')
-product_elements = soup.find_all('div', class_='set-card block')
